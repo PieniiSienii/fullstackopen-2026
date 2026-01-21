9d19f22ccc25c2a8526827ce06e305b793bd75c2
HerttaVierikko <hertta.vierikko@gmail.com>
2026-01-21 14:38:32 +0200

    Exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,18 +4,36 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allclicks, setSum] = useState(0)
+  const [averageList, setAverage] = useState([])
   
   const handleGoodClick = () => {
+    setAverage(averageList.concat(1))
     setGood(good + 1)
+    setSum(allclicks + 1)
   }
 
   const handleNeutral = () => {
     setNeutral(neutral + 1)
+    setSum(allclicks + 1)
+    setAverage(averageList.concat(0))
   }
 
   const handleBad = () => {
     setBad(bad +1)
+    setSum(allclicks + 1)
+    setAverage(averageList.concat(-1  ))
   }
+
+let average = 0
+let goodPercentage = 0
+
+if (averageList.length !== 0) {
+  const sum = averageList.reduce((a, b) => a + b, 0)
+  average = sum / averageList.length
+  const goodCount = averageList.filter(item => item === 1).length
+  goodPercentage = (goodCount / averageList.length) * 100
+}
 
   return (
     <div>
@@ -27,6 +45,9 @@ const App = () => {
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All {allclicks}</p>
+      <p>Average {average} </p>
+      <p>Positive % {goodPercentage.toFixed(2)}</p>
     </div>
   )
 }
