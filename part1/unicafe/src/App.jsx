67a1fea4f6155e24b96ba35bddc2f85780cc1002
HerttaVierikko <hertta.vierikko@gmail.com>
2026-01-22 09:59:08 +0200

    Finish Unicafe exercise

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,39 +1,52 @@
 import { useState } from 'react'
 
+const StatisticsLine = ({text, value}) => {
+  return <p>{text} {value}</p>
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  let average = 0
+  let goodPercentage = 0
+
+  if (all == 0) {
+    return <p>No feedback given</p>
+  }
+  
+  average = (good - bad) / all
+  goodPercentage = (good/ all) *100
+
+  return (
+    <div>
+      <StatisticsLine text="Good" value={good} />
+      <StatisticsLine text= "Neutral" value={neutral} />
+      <StatisticsLine text="Bad" value={bad} />
+      <StatisticsLine text="All" value={all} />
+      <StatisticsLine text="Average" value={`${average.toFixed(1)}`} />
+      <StatisticsLine text="Positive" value= {`${goodPercentage.toFixed(1)} %`}/>
+    </div>
+  )
+
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [allclicks, setSum] = useState(0)
-  const [averageList, setAverage] = useState([])
-  
+
+
   const handleGoodClick = () => {
-    setAverage(averageList.concat(1))
     setGood(good + 1)
-    setSum(allclicks + 1)
   }
 
   const handleNeutral = () => {
     setNeutral(neutral + 1)
-    setSum(allclicks + 1)
-    setAverage(averageList.concat(0))
   }
 
   const handleBad = () => {
     setBad(bad +1)
-    setSum(allclicks + 1)
-    setAverage(averageList.concat(-1  ))
   }
 
-let average = 0
-let goodPercentage = 0
-
-if (averageList.length !== 0) {
-  const sum = averageList.reduce((a, b) => a + b, 0)
-  average = sum / averageList.length
-  const goodCount = averageList.filter(item => item === 1).length
-  goodPercentage = (goodCount / averageList.length) * 100
-}
 
   return (
     <div>
@@ -42,12 +55,11 @@ if (averageList.length !== 0) {
       <button onClick={handleNeutral}>Neutral</button>
       <button onClick={handleBad}>Bad</button>
       <h1>Statistics</h1>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>All {allclicks}</p>
-      <p>Average {average} </p>
-      <p>Positive % {goodPercentage.toFixed(2)}</p>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+      />
     </div>
   )
 }
